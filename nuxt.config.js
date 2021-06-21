b925d542acaa0009ea01a9769ac45d1f807366f6
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-22 01:47:43 +0200

    feat(graphics): add graphics for home page

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,14 @@ export default {
   plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: [
+    '~/components/atoms/graphics',
+    '~/components/atoms/icons',
+    '~/components/atoms',
+    '~/components/molecules',
+    '~/components/organisms',
+    '~/components/templates',
+  ],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
