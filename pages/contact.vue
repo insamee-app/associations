e511ed42645eb5a49f6448389af72f439190e4ad
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-23 10:51:16 +0200

    feat(contat): create contact page

---
+++ pages/contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <InsameeContact :mail="mail">
+    <GraphicMail class="w-full" />
+  </InsameeContact>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mail: 'mailto:',
+    }
+  },
+}
+</script>
