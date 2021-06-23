d95d0dd7f0a02d74b8670ca80b1304d0daf13345
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-23 10:48:42 +0200

    feat(TheHeader): create TheHeader

---
+++ components/organisms/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <InsameeHeader
+    :icon-link="{ name: 'index' }"
+    icon-nav
+    @open="$emit('open', $event)"
+  >
+    <template #nav
+      ><InsameeHeaderNav> <InsameeAppList :list="nav" row /> </InsameeHeaderNav>
+    </template>
+    <template #actions>
+      <InsameeAppButton border :href="$config.insameeURL + '/signin'">
+        S'inscrire
+      </InsameeAppButton>
+    </template>
+  </InsameeHeader>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+  props: {
+    nav: {
+      type: Array,
+      default: () => [],
+    },
+  },
+}
+</script>
