fb9ba6bfa9ae5fdd4cc49e1509e062c414780706
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-23 22:51:31 +0200

    feat(package): install breakpoints

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,6 +55,7 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/content)
     '@nuxt/content',
+    'vue-screen/nuxt',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
