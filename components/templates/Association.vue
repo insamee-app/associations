863e2a62280f24caabc7f9c04b2b0bc94c9ea5c9
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-23 10:55:20 +0200

    feat(page): present an association

---
+++ components/templates/Association.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="space-y-8">
+    <AssociationInformation
+      :name="name"
+      :school-name="schoolName"
+      :image-url="imageUrl"
+      :thematic="thematic"
+      :thematics="thematics"
+      :text="text"
+    />
+    <div class="text-secondary-dark text-lg">Membres :</div>
+    <div v-if="profiles" class="space-y-8">
+      <InsameeProfileCard
+        v-for="profile in profiles"
+        :key="profile.id"
+        :last-name="profile.last_name"
+        :first-name="profile.first_name"
+        :current-role="profile.current_role"
+        :user-id="profile.user_id"
+        :text="profile.text"
+        :skills="profile.skills"
+        :associations="profile.associations"
+        :link="profile.link"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Association',
+  props: {
+    name: {
+      type: String,
+      default: undefined,
+    },
+    schoolName: {
+      type: String,
+      default: undefined,
+    },
+    imageUrl: {
+      type: String,
+      default: undefined,
+    },
+    thematic: {
+      type: String,
+      default: undefined,
+    },
+    thematics: {
+      type: Array,
+      default: undefined,
+    },
+    text: {
+      type: String,
+      default: undefined,
+    },
+    profiles: {
+      type: Array,
+      default: undefined,
+    },
+  },
+}
+</script>
---
+++ pages/associations/_id.vue
@@ -0,0 +1,53 @@
+<template>
+  <InsameeAppContainer>
+    <Association
+      :name="association.name"
+      :school-name="association.school.name"
+      :image-url="association.image_url"
+      :thematic="association.thematic"
+      :thematics="association.thematics"
+      :text="association.text"
+      :profiles="profiles"
+    />
+  </InsameeAppContainer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      association: {
+        name: 'lorem ipsum',
+        school: {
+          name: 'dolor',
+        },
+        image_url: undefined,
+        thematic: 'lorem them',
+        thematics: ['lorem', 'upsum', 'dolor', 'sit amet'],
+        text: 'Assocation est une association de musique. Elle fait partie du pôle culturel de l Insa Centre Val de Loire, et propose des Jam chaque Jeudi. Son président est grave beau.',
+      },
+      profiles: [
+        {
+          id: 1,
+          user_id: 1,
+          last_name: 'a last name',
+          first_name: 'a first name',
+          current_role: 'student',
+          text: 'lorem ipsum, this is the text',
+          link: undefined,
+        },
+        {
+          id: 2,
+          user_id: 1,
+          last_name: 'a last name',
+          first_name: 'a first name',
+          current_role: 'student',
+          text: 'lorem ipsum, this is the text',
+          skills: ['lorem', 'ipsum'],
+          link: undefined,
+        },
+      ],
+    }
+  },
+}
+</script>
