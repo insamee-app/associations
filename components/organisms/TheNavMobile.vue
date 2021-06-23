01406d8c67e232ca26274d0c87950c2750e2fc23
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-23 10:49:48 +0200

    feat(TheNavMobile): create the nav for mobile

---
+++ components/organisms/TheNavMobile.vue
@@ -0,0 +1,33 @@
+<template>
+  <InsameeNavMobile :list="nav" :value="value" @close="close">
+    <template #actions>
+      <InsameeAppButton border :href="$config.insameeURL + '/signin'">
+        S'inscrire
+      </InsameeAppButton>
+      <InsameeAppButton :href="$config.insameeURL + '/login'">
+        Se connecter
+      </InsameeAppButton>
+    </template>
+  </InsameeNavMobile>
+</template>
+
+<script>
+export default {
+  name: 'TheNavMobile',
+  props: {
+    value: {
+      type: Boolean,
+      required: true,
+    },
+    nav: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    close() {
+      this.$emit('close', false)
+    },
+  },
+}
+</script>
