ff486fe4fbcdb5c68f5476acd9b29004d653dcf6
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-06-23 10:52:39 +0200

    feat(layout): create minimal and error

---
+++ layouts/error.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="text-center">
+    <template v-if="error.statusCode === 404">
+      <Insamee404 :error="error" :to="{ name: 'index' }"> Acceuil </Insamee404>
+    </template>
+    <template v-else>
+      <InsameeErrorGlobal :error="error" :to="{ name: 'index' }">
+        Accueil
+      </InsameeErrorGlobal>
+    </template>
+  </section>
+</template>
+
+<script>
+export default {
+  layout: 'minimal',
+  props: {
+    error: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      show: false,
+    }
+  },
+}
+</script>
---
+++ layouts/minimal.vue
@@ -0,0 +1,8 @@
+<template>
+  <main class="mx-12 my-16">
+    <NuxtLink :to="{ name: 'index' }">
+      <InsameeIconInsamee class="w-40 mx-auto" />
+    </NuxtLink>
+    <Nuxt class="max-w-xs mx-auto" />
+  </main>
+</template>
