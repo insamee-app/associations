337030b62b0212dc44c25eabc81435d446979597
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-07-02 21:21:36 +0200

    fix(scrollBehavior): update

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -1,7 +1,9 @@
 export default function (to, from, savedPosition) {
-  return {
-    selector: 'header',
-    offset: { x: 0, y: -64 },
-    behavior: 'smooth',
-  }
+  if (from.name === 'associations' && to.name === 'associations')
+    return {
+      selector: 'header',
+      offset: { x: 0, y: -64 },
+      behavior: 'smooth',
+    }
+  else return { x: 0, y: 0 }
 }
